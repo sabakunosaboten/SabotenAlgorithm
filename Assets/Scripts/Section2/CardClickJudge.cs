b1ae484ddf7a2b908b62406e2880556e526db818
sabakunosaboten <sayuri922sayuri@gmail.com>
2026-06-14 14:57:20 +0900

    カード入れ替え処理できた！！SpriteRendererとかの型の扱いがわかってきてる気がする。うれしい

--- Assets/Scripts/Section2/CardClickJudge.cs
+++ Assets/Scripts/Section2/CardClickJudge.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CardClickJudge : MonoBehaviour
 {
     [SerializeField] Sprite[] cardSprites;
     [SerializeField]CopyObject CopyObjectScript;
+    bool canClick;
+    GrowCard previousTarget;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        canClick = true;
         for(int i = 0; i < cardSprites.Length; i++)
         {
             SpriteRenderer cardSprite = CopyObjectScript.cardInstance[i].GetComponent<SpriteRenderer>();
@@ -17,6 +21,42 @@ public class CardClickJudge : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(canClick == true)
+        {
+            if (Mouse.current.leftButton.wasPressedThisFrame)
+            {
+                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+                RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
+
+                // ★修正ポイント1：光線が何かに当たったか（空振りしていないか）をチェック！
+                if (hit.collider != null)
+                {
+                    ChangeImage targetCard = hit.collider.GetComponent<ChangeImage>();
+                    GrowCard target = hit.collider.GetComponent<GrowCard>();
+                    SpriteRenderer nowSprite = target.GetComponent<SpriteRenderer>();
+                    if(previousTarget != null)
+                    {
+                        previousTarget.Grow();
+                        SpriteRenderer previousSprite = previousTarget.GetComponent<SpriteRenderer>();
+                        Sprite kariSprite = previousSprite.sprite;
+                        previousSprite.sprite = nowSprite.sprite;
+                        nowSprite.sprite = kariSprite;
+                        previousSprite = null;
+                    }
+                    else
+                    {
+                        target.Grow();
+                        previousTarget = target;
+                    }
+
+                    // ★修正ポイント2：当たったものに「ChangeImage」が付いているかチェック！
+                    if (targetCard != null)
+                    {
+                    
+
+                    }
+                }
+            }
+        }
     }
 }
--- Assets/Scripts/Section2/Score200.cs
+++ Assets/Scripts/Section2/Score200.cs
@@ -5,40 +5,14 @@ using UnityEngine.InputSystem;
 public class Score200 : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    bool canClick;
-    GrowCard previousTarget;
     void Start()
     {
-        canClick = true;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(canClick == true)
-        {
-            if (Mouse.current.leftButton.wasPressedThisFrame)
-            {
-                Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-                RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero);
-
-                // ★修正ポイント1：光線が何かに当たったか（空振りしていないか）をチェック！
-                if (hit.collider != null)
-                {
-                    ChangeImage targetCard = hit.collider.GetComponent<ChangeImage>();
-                    GrowCard target = hit.collider.GetComponent<GrowCard>();
-                    target.Grow();
-                    if(previousTarget != null)previousTarget.Grow();
-                    previousTarget = target;
-
-                    // ★修正ポイント2：当たったものに「ChangeImage」が付いているかチェック！
-                    if (targetCard != null)
-                    {
-                    
-
-                    }
-                }
-            }
-        }
+        
     }
 }
